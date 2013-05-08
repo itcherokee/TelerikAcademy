2826c6754a23ac152014a46314f2b6f007faceea
Cherokee <simeonov23@yahoo.com>
2013-05-08 07:31:39 +0300

    code tunning update

---
+++ Programming/06.QualityCode/10.CodeTunningAndOptimization/02.SimpleMathPerformance/MathUtils.cs
@@ -0,0 +1,51 @@
+﻿namespace SimpleMathPerformance
+{
+    using System;
+
+    class MathUtils
+    {
+        internal static T Add<T>(T operandOne, T operandTwo) where T : struct
+        {
+            dynamic firstOperand = operandOne;
+            dynamic secondOperand = operandTwo;
+            T result = firstOperand + secondOperand;
+            return result;
+        }
+
+        internal static T Subtract<T>(T operandOne, T operandTwo) where T : struct
+        {
+            dynamic firstOperand = operandOne;
+            dynamic secondOperand = operandTwo;
+            T result = firstOperand - secondOperand;
+            return result;
+        }
+
+        internal static T Increment<T>(T operandOne, int incrementOffset) where T : struct
+        {
+            dynamic firstOperand = operandOne;
+            int secondOperand = incrementOffset;
+            for (int index = 0; index < incrementOffset; index++)
+            {
+                firstOperand++;
+            }
+
+            return firstOperand;
+        }
+
+        internal static T Multiply<T>(T operandOne, T operandTwo) where T : struct
+        {
+            dynamic firstOperand = operandOne;
+            dynamic secondOperand = operandTwo;
+            T result = firstOperand * secondOperand;
+            return result;
+        }
+
+        internal static T Divide<T>(T operandOne, T operandTwo) where T : struct
+        {
+            dynamic firstOperand = operandOne;
+            dynamic secondOperand = operandTwo;
+            T result = firstOperand / secondOperand;
+            return result;
+        }
+    }
+}
---
+++ Programming/06.QualityCode/10.CodeTunningAndOptimization/03.AdvancedMathPerformance/MathAdvanceUtils.cs
@@ -0,0 +1,28 @@
+﻿namespace AdvancedMathPerformance
+{
+    using System;
+
+    class MathAdvanceUtils
+    {
+        internal static T SquareRoot<T>(T operand) where T : struct
+        {
+            double firstOperand = (double)operand;
+            T result = (T)Math.Sqrt(firstOperand);
+            return result;
+        }
+
+        internal static T Logarithm<T>(T operand) where T : struct
+        {
+            double firstOperand = (double)operand;
+            T result = (T)Math.Log(firstOperand);
+            return result;
+        }
+
+        internal static T Sinus<T>(T angle) where T : struct
+        {
+            double angleOperand = (double)angle;
+            T result = (T)Math.Sin(angleOperand);
+            return result;
+        }
+    }
+}
