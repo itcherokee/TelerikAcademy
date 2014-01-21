886ec1c44264ce02e40a56575a175f0358bd5210
itCherokee <simeonov23@yahoo.com>
2014-01-21 14:50:21 +0200

    C#Part2 - Exam Preparation
    additional

--- Programming/02.CSharp-PartTwo/09.ExamPreparation/4 Feb 2013 - Morning/05.OneTaskIsNotEnough/OneTaskIsNotEnoughExec.cs
+++ Programming/02.CSharp-PartTwo/09.ExamPreparation/4 Feb 2013 - Morning/05.OneTaskIsNotEnough/OneTaskIsNotEnoughExec.cs
@@ -2,10 +2,7 @@
 namespace OneTaskIsNotEnough
 {
     using System;
-    using System.Collections.Generic;
     using System.Linq;
-    using System.Text;
-    using System.Threading.Tasks;
 
     public class OneTaskIsNotEnoughExec
     {
@@ -13,64 +10,59 @@ namespace OneTaskIsNotEnough
         {
             // Input
             int numberOfLamps = int.Parse(Console.ReadLine());
-            //StringBuilder moves = new StringBuilder();
             string firstMove = Console.ReadLine();
-
             string secondMove = Console.ReadLine();
 
             // Task 1
-            Console.WriteLine(WhichIsLastLapmTurnedOn(numberOfLamps) + 1);
+            Console.WriteLine(WhichIsLastLapmTurnedOn(numberOfLamps));
 
             // Task 2
-
             Console.WriteLine(IsBounded(firstMove));
-            Console.WriteLine(IsBounded(secondMove)); 
+            Console.WriteLine(IsBounded(secondMove));
         }
 
         private static int WhichIsLastLapmTurnedOn(int numberOfLamps)
         {
-            bool[] lamps = new bool[numberOfLamps];
+            int[] oldLamps = new int[numberOfLamps];
+            for (int i = 0; i < numberOfLamps; i++)
+            {
+                oldLamps[i] = i + 1;
+            }
+
             int lampsOffCounter = numberOfLamps;
             int lastLamp = 0;
-            int firstLampOff = 0;
-            int offset = 1;
-            while (lampsOffCounter != 0)
+            int offset = 2;
+            int[] lamps = new int[lampsOffCounter];
+            int lastElement = numberOfLamps;
+            while (lampsOffCounter > 0)
             {
-                for (int turnedOffIndex = firstLampOff; turnedOffIndex < numberOfLamps; turnedOffIndex++)
+                int index = 0;
+                int newIndex = 0;
+                while (index < lastElement)
                 {
-                    if (lamps[turnedOffIndex] == false)
+                    if (oldLamps[index] > 0)
                     {
-                        firstLampOff = turnedOffIndex;
-                        break;
+                        lamps[newIndex] = oldLamps[index];
+                        newIndex++;
                     }
+
+                    index++;
                 }
 
-                int stepPassed = offset;
-                // TODO: trqybva da go preprabotya na while i da go prekysvam po-rano ako do kraya ima po-malko lampi
-                for (int i = firstLampOff; i < numberOfLamps; i++)
+                lastElement = lampsOffCounter;
+                if (lampsOffCounter == 1)
                 {
-                    if (!lamps[i])
-                    {
-                        if (firstLampOff == i)
-                        {
-                            lamps[i] = true;
-                            lastLamp = i;
-                            lampsOffCounter--;
-                            continue;
-                        }
+                    lastLamp = lamps[0];
+                    break;
+                }
 
-                        if (lamps[i] == false && stepPassed > 0)
-                        {
-                            stepPassed--;
-                        }
-                        else if (lamps[i] == false)
-                        {
-                            lamps[i] = true;
-                            lastLamp = i;
-                            lampsOffCounter--;
-                            stepPassed = offset;
-                        }
-                    }
+                oldLamps = lamps;
+                int temp = lampsOffCounter;
+                for (int i = 0; i < temp; i += offset)
+                {
+                    lastLamp = oldLamps[i];
+                    oldLamps[i] = -1;
+                    lampsOffCounter--;
                 }
 
                 offset++;
@@ -81,88 +73,14 @@ namespace OneTaskIsNotEnough
 
         private static string IsBounded(string c)
         {
-            // int result = 0;
-
-            int r = 0;// = moves.Count(x => x == 'R');
-            int l = 0;// = moves.Count(x => x == 'L');
-
-
-            //   foreach (var c in moves)
-
-            //  StringBuilder c = new StringBuilder(moves);
-            for (int i = 0; i < c.Length; i++)
-            {
-                if (c[i] != 'S')
-                {
-                    if (c[i] == 'L')
-                    {
-                        l++;
-                    }
-                    else
-                    {
-                        r++;
-                    }
-                    //l += c.Equals('L') ? 1 : 0;
-                    //r += c.Equals('R') ? 1 : 0; 
-                }
-            }
-
+            int r = c.Count(x => x == 'R');
+            int l = c.Count(x => x == 'L');
             if (l - r < 0 || l - r > 0)
             {
                 return "bounded";
             }
-            else
-            {
-                return "unbounded";
-            }
 
-            //char firstTurn = 'S';
-            //for (int i = 0; i < moves.Length; i++)
-            //{
-            //    switch (moves[i])
-            //    {
-            //        case 'R':
-            //            if (firstTurn.Equals('S'))
-            //            {
-            //                firstTurn = 'R';
-            //                result++;
-            //            }
-            //            else
-            //            {
-            //                if (firstTurn.Equals('R'))
-            //                {
-            //                    result++;
-            //                }
-            //                else
-            //                {
-            //                    result--;
-            //                }
-            //            }
-
-            //            break;
-            //        case 'L':
-            //            if (firstTurn.Equals('S'))
-            //            {
-            //                firstTurn = 'L';
-            //                result++;
-            //            }
-            //            else
-            //            {
-            //                if (firstTurn.Equals('L'))
-            //                {
-            //                    result++;
-            //                }
-            //                else
-            //                {
-            //                    result--;
-            //                }
-            //            }
-
-            //            break;
-            //    }
-            //}
-
-            //return result > 0 ? true : false;
+            return "unbounded";
         }
     }
 }
